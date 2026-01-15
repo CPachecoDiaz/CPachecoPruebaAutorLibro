1fb44240881df85a146444316b261c5b37dacdc4
CPachecoDiaz <carlapachecodl@hotmail.com>
2026-01-15 16:16:00 -0600

    Agregando Modelos en ML

---
+++ ML/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Libro
+    {
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public string FechaPublicacion { get; set; }
+        public ML.Editorial Editorial { get; set; }
+    }
+}
